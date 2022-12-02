a5c4493022f4f418ec9c91561812bc239587e163
Juan Luis Fragoso del Rey <juanluisfragoso@gmail.com>
2022-12-02 17:40:13 +0100

    first commit

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useDeferredValue, useState } from 'react';
 
 export const CounterApp = ({ value }) => {
 
-  const [ counter, setCounter ] = useState(10);
+  console.log('render');
+
+  const [ counter, setCounter ] = useState(value);
+  
   const handleAdd = () => {
-    setCounter ( counter + 1);
+    setCounter ( (c) => c+1);
+  }
+
+  const handleMinus = () => {
+    setCounter ( (c) => c-1);
+  }
+  
+  const handleReset = () => {
+    setCounter ( useDeferredValue);
   }
 
   return (
@@ -13,6 +24,8 @@ export const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleMinus}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
