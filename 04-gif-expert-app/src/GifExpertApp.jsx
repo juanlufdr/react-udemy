259c5538b069cd297a095c770e3ee396fa5817f2
Juan Luis Fragoso del Rey <juanluisfragoso@gmail.com>
2022-12-09 21:31:11 +0100

    first commit

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -5,6 +5,7 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories([newCategory, ...categories]);
   };
 
