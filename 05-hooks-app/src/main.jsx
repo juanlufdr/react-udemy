75781b3244d7ef2d2ec6fd448b9d346a8d1e43c5
Juan Luis Fragoso del Rey <juanluisfragoso@gmail.com>
2022-12-11 15:28:42 +0100

    fc

--- 05-hooks-app/src/main.jsx
+++ 05-hooks-app/src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
-import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
-    <CounterWithCustomHook />
+    {/* <CounterWithCustomHook /> */}
+    <SimpleForm />
   </React.StrictMode>
 );
